f11a3c730758dc788e8c2935efb8433f0e6c1ffa
Robson Leal ArchPc <devleal@hotmail.com>
2022-02-05 19:24:13 -0300

    Criando serializer para listar alunos matriculados em um curso e listar cursos em que um aluno esta matriculado

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
 
@@ -8,10 +9,25 @@ class AlunoSerializer(serializers.ModelSerializer):
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
-        model: Curso
+        model = Curso
         fields = '__all__'
 
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
-        model: Matricula
+        model = Matricula
         exclude = []
+
+class ListaMatriculaAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno']
